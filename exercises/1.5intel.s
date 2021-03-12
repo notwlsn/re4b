4e8f46cc95ca1fd960db7634e5c92e7f7e6c6660
notwlsn <gwillson.camp@gmail.com>
2021-03-12 13:03:22 -0500

    adding exercises folder back

---
+++ exercises/1.5intel.s
@@ -0,0 +1,59 @@
+	.file	"1.5.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"hello,world"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	lea	ecx, 4[esp]
+	.cfi_def_cfa 1, 0
+	and	esp, -16
+	push	DWORD PTR -4[ecx]
+	push	ebp
+	.cfi_escape 0x10,0x5,0x2,0x75,0
+	mov	ebp, esp
+	push	ebx
+	push	ecx
+	.cfi_escape 0xf,0x3,0x75,0x78,0x6
+	.cfi_escape 0x10,0x3,0x2,0x75,0x7c
+	call	__x86.get_pc_thunk.bx
+	add	ebx, OFFSET FLAT:_GLOBAL_OFFSET_TABLE_
+	sub	esp, 12
+	lea	eax, .LC0@GOTOFF[ebx]
+	push	eax
+	call	puts@PLT
+	add	esp, 16
+	lea	esp, -8[ebp]
+	xor	eax, eax
+	pop	ecx
+	.cfi_restore 1
+	.cfi_def_cfa 1, 0
+	pop	ebx
+	.cfi_restore 3
+	pop	ebp
+	.cfi_restore 5
+	lea	esp, -4[ecx]
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.section	.text.__x86.get_pc_thunk.bx,"axG",@progbits,__x86.get_pc_thunk.bx,comdat
+	.globl	__x86.get_pc_thunk.bx
+	.hidden	__x86.get_pc_thunk.bx
+	.type	__x86.get_pc_thunk.bx, @function
+__x86.get_pc_thunk.bx:
+.LFB24:
+	.cfi_startproc
+	mov	ebx, DWORD PTR [esp]
+	ret
+	.cfi_endproc
+.LFE24:
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
